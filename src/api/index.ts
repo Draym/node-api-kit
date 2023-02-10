2d3d292a54a4acdeded3b9bd3033da9499aeea1b
Draym <kevin.draym@gmail.com>
2023-02-10 11:21:10 +0800

    task: add regex export

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,8 @@
+import ApiMethodAccess from "./apimethod-access"
+import Auth from "./auth"
+
+export {
+    Auth,
+    ApiMethodAccess
+}
 export * from "./api.handler"
-export * from "./apimethod-access"
-export * from "./auth"
