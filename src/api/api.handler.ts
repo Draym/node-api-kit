857aa429f3abad6718cef3c41891282f78f6e2ef
Draym <kevin.draym@gmail.com>
2023-02-20 16:47:46 +0800

    task: add optional res param into handle

--- src/api/api.handler.ts
+++ src/api/api.handler.ts
@@ -3,10 +3,10 @@ import {isNull, throwIf} from "../utils/checks"
 import {empty, Response, success} from "../interfaces"
 import {Errors} from "../utils/errors"
 
-export async function handle<Req, Res>(this: (request: Req) => Promise<Res>, req: Req, res: Response<Res>, next: NextFunction) {
+export async function handle<Req, Res>(this: (request: Req, res?: Response<Res>) => Promise<Res>, req: Req, res: Response<Res>, next: NextFunction) {
     try {
         throwIf(typeof this !== 'function', Errors.NOT_IMPLEMENTED("missing handler"))
-        const result = await this(req)
+        const result = await this(req, res)
         if (isNull(result)) {
             empty(res)
         } else {
