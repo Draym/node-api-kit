1792e51a555922b40f0f93a9d12297b409154b90
Draym <kevin.draym@gmail.com>
2023-02-28 15:35:11 +0800

    task: add new auth interface

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -15,7 +15,13 @@ export interface AuthData {
   apiKey: DataStoredInApiKey | null
 }
 
+export interface AuthRawData {
+  token: string | null
+  apiKey: string | null
+}
+
 export interface CallerData {
   id: number
   uuid: string
+  email?: string
 }
