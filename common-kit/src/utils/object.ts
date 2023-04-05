ef44e4da09c1a823f543de70cfe26d9c2f3640ae
Draym <kevin.draym@gmail.com>
2023-04-05 17:10:22 +0800

    task: merge object can ignore empty

--- common-kit/src/utils/object.ts
+++ common-kit/src/utils/object.ts
@@ -1,10 +1,16 @@
-import {isNotNull} from "./checks"
+import {isNotEmpty, isNotNull, isNull} from "./checks"
 
-export function merge(d1: {}, d2: {}): any {
+/**
+ * Merges two objects. If the same key is present in both objects, the value from the second object is used.
+ * @param d1 the first object
+ * @param d2 the second object
+ * @param ignoreEmpty if true then empty values are ignored and do not replace any value (except null)
+ */
+export function merge(d1: {}, d2: {}, ignoreEmpty = false): any {
     const result = {...d1}
 
     for (const key in d2) {
-        if (isNotNull(d2[key])) {
+        if (isNull(result[key]) || (ignoreEmpty && isNotEmpty(d2[key])) || isNotNull(d2[key])) {
             let value
             if (typeof d2[key] === 'object' && isNotNull(d1[key]) && typeof d1[key] === 'object') {
                 value = merge(d1[key], d2[key])
